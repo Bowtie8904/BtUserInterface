343312d9ba31a05867da03d45e29ddb667aa6648
Bowtie8904 <lukas.hartwig@vanderlande.com>
2019-10-22 14:57:15 +0200

    Added target and retention annotations.

--- src/bt/gui/fx/core/FxmlElement.java
+++ src/bt/gui/fx/core/FxmlElement.java
@@ -1,9 +1,16 @@
 package bt.gui.fx.core;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author &#8904
  *
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface FxmlElement
 {
     /**

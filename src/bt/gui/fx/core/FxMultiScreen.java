b4a6429c18f65e318182f4aa434f709f12308b86
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2020-04-12 11:01:27 +0200

    Added MultiScreen to better manage screens that consist of multiple subscreens.

---
+++ src/bt/gui/fx/core/FxMultiScreen.java
@@ -0,0 +1,128 @@
+package bt.gui.fx.core;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import bt.gui.fx.core.exc.FxException;
+import bt.utils.log.Logger;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author &#8904
+ *
+ */
+public abstract class FxMultiScreen extends FxScreen
+{
+    private Map<Class<? extends FxScreen>, FxScreen> screens;
+
+    public FxMultiScreen()
+    {
+        super();
+        loadScreens();
+    }
+
+    public <T extends FxScreen> void addScreens(Class<T>... screenTypes)
+    {
+        for (Class<T> type : screenTypes)
+        {
+            T screen = constructScreenInstance(type);
+            this.screenManager.addScreen(type, screen);
+        }
+    }
+
+    public <T extends FxScreen> T getScreen(Class<T> screenType)
+    {
+        return (T)this.screens.get(screenType);
+    }
+
+    @Override
+    public Parent load()
+    {
+        super.load();
+
+        for (FxScreen screen : this.screens.values())
+        {
+            screen.load();
+        }
+
+        return this.root;
+    }
+
+    /**
+     * @see bt.gui.fx.core.FxScreen#prepareScreen()
+     */
+    @Override
+    protected void prepareScreen()
+    {
+        for (FxScreen screen : this.screens.values())
+        {
+            screen.prepareScreen();
+        }
+    }
+
+    /**
+     * @see bt.gui.fx.core.FxScreen#prepareStage(javafx.stage.Stage)
+     */
+    @Override
+    protected void prepareStage(Stage stage)
+    {
+        for (FxScreen screen : this.screens.values())
+        {
+            screen.setStage(stage);
+            screen.prepareStage(stage);
+        }
+
+        setupStage(stage);
+    }
+
+    /**
+     * @see bt.gui.fx.core.FxScreen#prepareScene(javafx.scene.Scene)
+     */
+    @Override
+    protected void prepareScene(Scene scene)
+    {
+        for (FxScreen screen : this.screens.values())
+        {
+            screen.setScene(scene);
+            screen.prepareScene(scene);
+        }
+
+        setupScene(scene);
+        finishSetup();
+    }
+
+    private <T extends FxScreen> T constructScreenInstance(Class<T> screenType)
+    {
+        T screen = null;
+
+        try
+        {
+            Constructor<T> construct = screenType.getConstructor();
+            construct.setAccessible(true);
+            screen = construct.newInstance();
+            this.screens.put(screenType, screen);
+        }
+        catch (InstantiationException | IllegalAccessException
+               | InvocationTargetException | SecurityException e1)
+        {
+            Logger.global().print(e1);
+        }
+        catch (NoSuchMethodException noEx)
+        {
+            throw new FxException("Screen class must implement a constructor without arguments.");
+        }
+
+        return screen;
+    }
+
+    protected abstract void loadScreens();
+
+    protected abstract void setupStage(Stage stage);
+
+    protected abstract void setupScene(Scene scene);
+
+    protected abstract void finishSetup();
+}

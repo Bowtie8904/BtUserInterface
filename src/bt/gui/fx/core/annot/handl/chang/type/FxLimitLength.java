a70b40247e5fa4d60ee7306de8c38e0e8af4cbd6
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2020-04-18 00:01:24 +0200

    Now using additionValue for limiting text length.

--- src/bt/gui/fx/core/annot/handl/chang/type/FxLimitLength.java
+++ src/bt/gui/fx/core/annot/handl/chang/type/FxLimitLength.java
@@ -1,27 +1,51 @@
 package bt.gui.fx.core.annot.handl.chang.type;
 
-import bt.gui.fx.core.comp.BtTextField;
+import bt.gui.fx.core.exc.FxException;
 import javafx.beans.value.ChangeListener;
+import javafx.scene.control.TextInputControl;
 
 /**
  * @author &#8904
  *
  */
-public class FxLimitLength extends FxStringChange<BtTextField, String>
+public class FxLimitLength extends FxStringChange<TextInputControl, String>
 {
     /**
      * @see bt.gui.fx.core.annot.handl.chang.FxChangeHandlerType#getSpecialListener(java.lang.Object, java.lang.Object,
      *      java.lang.String)
      */
     @Override
-    protected ChangeListener<String> getSpecialListener(BtTextField fieldObj, Object handlingObj, String handlerMethodName, boolean withParameters, boolean passField, String additionalValue)
+    protected ChangeListener<String> getSpecialListener(TextInputControl fieldObj, Object handlingObj, String handlerMethodName, boolean withParameters, boolean passField, String maxLengthValue)
     {
-        return (obs, ol, ne) ->
+        ChangeListener<String> listener = null;
+
+        if (maxLengthValue.isEmpty())
         {
-            if (ne.length() > fieldObj.getMaxTextLength())
+            throw new FxException("Missing length limitation in 'value' field of FxHandler annotation.");
+        }
+
+        try
+        {
+            int maxLength = Integer.parseInt(maxLengthValue);
+
+            if (maxLength < 0)
             {
-                fieldObj.setText(ne.substring(0, fieldObj.getMaxTextLength()));
+                throw new FxException("Invalid length limitation in 'value' field of FxHandler annotation. Expected positive integer and got '" + maxLengthValue + "'.");
             }
-        };
+
+            listener = (obs, ol, ne) ->
+            {
+                if (ne.length() > maxLength)
+                {
+                    fieldObj.setText(ne.substring(0, maxLength));
+                }
+            };
+        }
+        catch (NumberFormatException e)
+        {
+            throw new FxException("Invalid length limitation in 'value' field of FxHandler annotation. Expected positive integer and got '" + maxLengthValue + "'.");
+        }
+
+        return listener;
     }
 }

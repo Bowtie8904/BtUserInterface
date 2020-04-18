c4737f959df57104032219ea4fdc48e66ca7b4d6
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2020-04-18 09:18:35 +0200

    Added buttonlistcell.

---
+++ src/bt/gui/fx/core/comp/ButtonListCell.java
@@ -0,0 +1,42 @@
+package bt.gui.fx.core.comp;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+
+/**
+ * @author &#8904
+ *
+ */
+public class ButtonListCell<T> extends HBox
+{
+    private T item;
+
+    public ButtonListCell(T item, Button... buttons)
+    {
+        this.item = item;
+        Label label = new Label(item.toString());
+
+        label.setMaxWidth(Double.MAX_VALUE);
+        HBox.setHgrow(label, Priority.ALWAYS);
+        getChildren().add(label);
+
+        addButtons(buttons);
+    }
+
+    public void addButtons(Button... buttons)
+    {
+        for (var b : buttons)
+        {
+            HBox.setMargin(b, new Insets(0, 2, 0, 0));
+            getChildren().add(b);
+        }
+    }
+
+    public T getItem()
+    {
+        return this.item;
+    }
+}
